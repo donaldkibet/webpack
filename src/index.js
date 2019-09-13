e6ad5c99ebf0e3d040778f4b787a32143abcc7fa
Donald Kibet <chelashawdonald@yahoo.com>
2019-09-13 16:49:25 +0300

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack']);
+    element.classList.add('hello');
+
+    const myicon = new Image();
+    myicon.src = Icon;
+    
+    element.appendChild(myicon);
+
+    _.map(Data,(_data)=>{
+        console.log(data);
+    });
 
     return element;
 }
